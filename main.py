f9a798a59790a41e12ecddedadf3cfbf95cd2e45
Sevinch <ysevinchoy2197@gmail.com>
2026-05-13 15:12:14 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# 14
+parol = input("parol kiriting: ")
+if len(parol) >= 6:
+    print("to'g'ri!")
+else:
+    print("6 ta belgidan kam!")
+
+
+
+
+
+
+
+
+# 15
+bal = int(input("balingizni kiriting: "))
+if bal >= 60 or bal >= 90:
+    print("a'lochi")
+else:
+    print("a'lochi emas!")
